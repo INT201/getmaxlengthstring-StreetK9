5fc391456a5aca79dd8dec54e26c159bf4970150
Pichaya Chantrasriwong <pichaya.cha@mail.kmutt.ac.th>
2022-09-28 14:14:44 +0700

    commit1

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,42 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here
+  if (arrayOfString === null || arrayOfString === undefined) {
+    return undefined
+  }else {
+    let maxWord = 0
+    let outPush = []
+    for (let i = 0; i < arrayOfString.length; i++) {
+       if(arrayOfString[i].length >= maxWord){
+        maxWord = arrayOfString[i].length  
+       }
+    }
+    for (i = 0; i < arrayOfString.length; i++) {
+      if (arrayOfString[i].length >= maxWord) {
+        outPush.push(arrayOfString[i])
+      }
+    }
+    return outPush
+  }
 }
+
+
 module.exports = getMaxLengthString
+
+
+/*
+# test-getMaxLengthString
+
+#### ให้เขียน Function ชื่อ getMaxLengthString (arrayOfString) เพื่อ return array ของ String 
+เฉพาะที่มีความยาวของจำนวนตัวอักษรมากที่สุด 
+
+กรณี arrayOfString มีค่าเป็น null หรือ undefined ให้ return undefined
+
+- ตัวอย่างเช่น
+  1. getMaxLengthString(['hello', 'hi', 'hey']) return ['hello']
+  2. getMaxLengthString(['hello', 'hi', 'hey', 'Good Evening', 'Good Morning']) return ['Good Evening', 'Good Morning']
+  3. getMaxLengthString(['hi']) return ['hi']
+  4. getMaxLengthString(null) return undefined
+  5. getMaxLengthString(undefined) return undefined
+
+  */
